18d4afc64ea278f2fc16970f41bec748a9957b7f
Sabbir Mahmud Afridi <smahmuddz@gmail.com>
2023-09-25 03:25:26 +0600

    Fixed text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className="container header__container">
           <h5>Hello, I'm</h5>
           <h1>Sabbir Mahmud Afridi</h1>
-          <h5 className="text-light">Software Engineering Graduate</h5>
+          <h5 className="text-light">Computer Science and Engineering Graduate</h5>
           <CTA/>
           <HeaderSocials/>
           <div className="me">
